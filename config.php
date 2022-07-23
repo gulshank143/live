0e8e963e4cb0f30e718fae4f382f05472e70543d
ravikaranrathore <singhravikaranrathore@gmail.com>
2022-07-24 05:21:52 +0530

    added more files for login page

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// connection to database !!!!
+$host = 'localhost';        
+$user = 'root';
+$pass = '';
+
+$db = 'car';        // database name
+
+$conn = mysqli_connect($host,$user,$pass,$db);
+
+
+?>
