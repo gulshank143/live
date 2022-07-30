3bc7714d05a0690ec578304ead53995b05a5c20a
ravikaranrathore <singhravikaranrathore@gmail.com>
2022-07-30 16:46:59 +0530

    ,

--- index.php
+++ index.php
@@ -9,10 +9,6 @@
         require_once 'header.php';
     ?>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a3f1e7334ecf43575fdf0cb650da93ed37ac7161
 <h6>
     <?php
         require_once 'form.php';
@@ -24,26 +20,6 @@
     <?php
         require_once 'footer.php';
     ?>
-</h6>
-<<<<<<< HEAD
-=======
-<h1> BODY to be Inserted </h1>
-<h1> BODY to be Inserted </h1>
-<h1> BODY to be Inserted </h1>
-<h1> BODY to be Inserted </h1>
-<h1> BODY to be Inserted </h1>
-<h1> BODY to be Inserted </h1>
-<h1> BODY to be Inserted </h1>
-<h1> BODY to be Inserted </h1>
-<h1> BODY to be Inserted </h1>
-<h1> BODY to be Inserted </h1>
-
-<?php
-require_once 'footer.php';
-?>
->>>>>>> 5a82d634fb8d159cefc022c319842a900d8e03da
-=======
->>>>>>> a3f1e7334ecf43575fdf0cb650da93ed37ac7161
 
 </body>
 </html>
